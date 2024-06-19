50636d806f01028f16d9181a60c1a485097d0cbd
Isabelichristo <164278908+Isabelichristo@users.noreply.github.com>
2024-06-19 23:49:49 +0000

    19/jun/24

--- script.js
+++ script.js
@@ -4,61 +4,64 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [ 
-  {
-    enunciado:"Qual é a cor dos seus olhos ?",
-    alternativas:[
-      "Azuis",
-      "verdes",
-    ]
-    },
-  {
-  enunciado: "Pergunta 2"
-    alternativas:[
-  "Resposta 1",
-  "Resposta 2",
-    ]
-  },
-{
-  enunciado:"Qual é a cor dos seus olhos ?",
-  alternativas:[
-    "Azuis",
-    "verdes",
-  ]
-  },
-  {
-    enunciado:"Qual é a cor dos seus olhos ?",
-    alternativas:[
-      "Azuis",
-      "verdes",
-    ]
-    },
+const perguntas = [
     {
-      enunciado:"Qual é a cor dos seus olhos ?",
-      alternativas:[
-        "Azuis",
-        "verdes",
+      enunciado: "assim que você saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele também gera imagens e aúdios super realistas. Qual o seu primeiro pensamento?",
+      alternativas: [
+        "Isso é assustador",
+        "Isso é maravilhoso"
       ]
-      },
+    },
+
+    {
+      enunciado: "Com a descoberta desta tecnologia, chamda inteligencia artifical (IA), uma prfessora de tecnoloogia de uma escola decidiu fazer muma sequência de aulas sobre essa ferramenta. No fim de uma aula ela pede que você escreva uma trabalho sobre o uso da IA em sala de aula. Qual atitude você toma?",
+      alternativas: [
+        "utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique em uma linguagem que facilite o entendimnto.",
+        "escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos proprios sobre o tema."
+      ]
+    },
+
+    {
+      enunciado: "Pergunta 3",
+      alternativas: [
+        "Resposta 1",
+        "Resposta 2"
+      ]
+    },
+
+    {
+      enunciado: "Pergunta 4",
+      alternativas: [
+        "Resposta 1",
+        "Resposta 2"
+      ]
+    },
+
+    {
+      enunciado: "Pergunta 5",
+      alternativas: [
+        "Resposta 1",
+        "Resposta 2"
+      ]
+    }
 ];
 
 let atual = 0;
-let perguntAtual = perguntas[atual];
+let perguntaAtual;
 
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativa = document.createElement("button");
-  botaoAlternativas.textContent= alternativa;
-  caixaAlternativas.appendChild(botaoAlternativa);
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
 
 
 function mostraPergunta() {
-perguntAtual = perguntas[atual];
-  caixaPerguntas.textContent=perguntAtual.enunciado;
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
   mostraAlternativas();
-   }
+}
+
 mostraPergunta();
-
-
